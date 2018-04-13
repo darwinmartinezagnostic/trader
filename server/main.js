b9349ddc264af1ddcbecb0a98cb8bfea32161c1e
darwinmartinezagnostic <darwin.martinez@agnosticit.com>
2018-04-13 13:35:48 -0300

    Implementacion de calculo de ganancia

--- server/main.js
+++ server/main.js
@@ -78,6 +78,7 @@ Meteor.methods({
         console.log('        ',fecha._d);
         console.log('############################################');
         console.log(' ');
+        Meteor.call("CalculoGanancia");
     },
 
     'FinEjecucion':function(){
@@ -2767,9 +2768,53 @@ Meteor.methods({
         return 0;
     },
 
+    'CalculoGanancia':function(){
+
+
+        var inversionInicial = GananciaPerdida.findOne({},{_id:0, inversion:1,ganancia:1},{ $sort: { fecha : -1 }});
+        var sum = 0;
+
+        console.log(inversionInicial);
+
+        var monedasSaldo = Monedas.aggregate([{ $project : { moneda:1, "saldo.tradeo.activo": 1, _id: 0 } },{ $match: { "saldo.tradeo.activo" : { $gt : 0 } }}])
+
+        for(var i = 0; i<monedasSaldo.length; i++) {
+            console.log(monedasSaldo[i].moneda);
+            console.log(monedasSaldo[i].saldo.tradeo.activo);
+            if(monedasSaldo[i].moneda != 'USD')
+                sum += Meteor.call("EquivalenteDolar",monedasSaldo[i].moneda,monedasSaldo[i].saldo.tradeo.activo);
+            else
+                sum += monedasSaldo[i].saldo.tradeo.activo;
+            console.log(sum);
+        }
+
+        if(inversionInicial != null){
+
+            GananciaPerdida.insert({fecha: new Date(), inversionAnterior:inversionInicial.inversion,inversion:sum,ganancia:sum-inversionInicial.inversion,comentario:'Ganancia a la fecha'});
+
+        }else {
+            GananciaPerdida.insert({fecha: new Date(), inversion:sum,ganancia:0,comentario:'Inversion inicial'});
+        }
+
+    },
+
+    'EquivalenteDolar':function(moneda,saldo){
+
+        var precioAux = TiposDeCambios.find({tipo_cambio:moneda+'USD'}).fetch();
+
+        if(precioAux.length === 0) {
+            precioAux = TiposDeCambios.find({tipo_cambio:moneda+'BTC'}).fetch();
+            saldo *= parseFloat(precioAux[0].periodo1.precio);
+            precioAux = TiposDeCambios.find({tipo_cambio:'BTCUSD'}).fetch();
+        }
+
+        return parseFloat(precioAux[0].periodo1.precio)*saldo;
+    },
+
     'EjecucionGlobal':function(){
         //Meteor.call("Encabezado");
-        Meteor.call("Prueba");
+        //Meteor.call("Prueba");
+        Meteor.call("CalculoGanancia");
         //var tiposCamb = ["XMRUSD", "XMRETH", "XMRBTC"];
         //Meteor.call("Invertir", tiposCamb, 2, 0);
         //Meteor.call("SaldoActualMonedas", 1);
