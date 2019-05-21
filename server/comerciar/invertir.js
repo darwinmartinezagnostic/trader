9b32c6ca0e97124fe73c0b0fb7880f0f56c05bfa
jarruiz <jarruizjesus@gmail.com>
2019-05-20 20:59:22 -0400

    Optimizacion de Secuencia

--- server/comerciar/invertir.js
+++ server/comerciar/invertir.js
@@ -578,7 +578,7 @@ Meteor.methods({
             console.log(" Valor de TmpTCMC: ", TmpTCMC)
             for (CTMCB = 0, T_TmpTCMB = TmpTCMB.length; CTMCB < T_TmpTCMB; CTMCB++) {
                 var V_TmpTCMB = TmpTCMB[CTMCB];
-                console.log("Valor de V_TmpTCMB", V_TmpTCMB)
+                //console.log("Valor de V_TmpTCMB", V_TmpTCMB)
                 TmpTipCambioXMonedaReord.insert({ "tipo_cambio": V_TmpTCMB.tipo_cambio,
                                                     "moneda_base": V_TmpTCMB.moneda_base,
                                                     "moneda_cotizacion" : V_TmpTCMB.moneda_cotizacion, 
@@ -598,7 +598,7 @@ Meteor.methods({
             
             for (CTMCB = 0, T_TmpTCMB = TmpTCMC.length; CTMCB < T_TmpTCMB; CTMCB++) {
                 var V_TmpTCMC = TmpTCMC[CTMCB];
-                console.log("Valor de V_TmpTCMC", V_TmpTCMC)
+                //console.log("Valor de V_TmpTCMC", V_TmpTCMC)
                 TmpTipCambioXMonedaReord.insert({ "tipo_cambio": V_TmpTCMC.tipo_cambio,
                                                     "moneda_base": V_TmpTCMC.moneda_base,
                                                     "moneda_cotizacion" : V_TmpTCMC.moneda_cotizacion, 
